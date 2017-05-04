681cddfb6167e3f3f6ac9e7c6ba2999f42c6ca3a
Chris Najman <christopher.najman@gmail.com>
2017-05-04 16:21:12 +0100

    Completed hero block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+pxtorem = require('postcss-pxtorem');
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([
+    	cssImport, 
+    	mixins, 
+    	pxtorem({
+            replace: true,
+            /*
+            If you change replace to 'false' it will generate
+            a pixel fallback for older browsers
+        	*/
+            propList: ['*']
+            /*
+				Using the '*' means that every pixel value (except for min and
+				max width in a media query) will be converted to rems
+            */
+        }), 
+        cssvars, 
+        nested, 
+        autoprefixer])
+    )
+    
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
