3787b10d97894f84505458f3c1f945a7aac45a87
Chris Najman <christopher.najman@gmail.com>
2017-05-13 17:45:42 +0100

    Scroll and reveal completed

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,12 @@
 import TheMobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
+
+
 var mobMenu = new TheMobileMenu();
+// var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
 
 // import FadeInPullUp from "./modules/FadeInUp";
 // var fadeinPullup = new FadeInPullUp();
