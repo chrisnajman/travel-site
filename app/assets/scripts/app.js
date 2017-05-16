b628727a4dbddfaa62fc312381779e1cf7333ded
Chris Najman <christopher.najman@gmail.com>
2017-05-16 11:49:48 +0100

    Final tweak to Sticky Header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,14 @@
 import TheMobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 
 var mobMenu = new TheMobileMenu();
 // var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
 
 // import FadeInPullUp from "./modules/FadeInUp";
 // var fadeinPullup = new FadeInPullUp();
