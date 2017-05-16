0109f7b412216a0ccf29003a03e314178dc4b75a
Chris Najman <christopher.najman@gmail.com>
2017-05-16 17:08:00 +0100

    Finish modal functionality and styling

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,6 +2,7 @@ import TheMobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 
 var mobMenu = new TheMobileMenu();
@@ -9,6 +10,8 @@ var mobMenu = new TheMobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+
+var modal = new Modal();
 
 // import FadeInPullUp from "./modules/FadeInUp";
 // var fadeinPullup = new FadeInPullUp();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,41 @@
+import $ from "jquery";
+
+class Modal {
+	constructor() {
+		this.openModalButton = $(".open-modal");
+		this.modal = $(".modal");
+		this.closeModalButton = $(".modal__close");
+		this.events();
+	}
+	
+	events() {
+		// clicking open modal button
+		this.openModalButton.click(this.openModal.bind(this));
+
+		// click the x close modal button
+		this.closeModalButton.click(this.closeModal.bind(this));
+
+		// if push any key
+		$(document).keyup(this.keyPressHandler.bind(this));
+
+	}
+
+	// 27 == Escape key
+	keyPressHandler(e) {
+		if (e.keyCode == 27) {
+			this.closeModal();
+		}
+	}
+	
+	openModal() {
+	 this.modal.addClass("modal--is-visible");
+	 return false;
+	}
+
+	closeModal() {
+	 this.modal.removeClass("modal--is-visible");
+	}
+
+}
+
+export default Modal;
